788be87951a43dbee229723683d2f8632dbcad5d
Sergioaquiles <sergioaquiles@hotmail.com>
2022-01-30 13:42:14 -0300

    Initial Commit

---
+++ DesafioBTG01/DesafioBTG01App.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioBTG01App.swift
+//  DesafioBTG01
+//
+//  Created by Sergio Cardoso on 30/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DesafioBTG01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
